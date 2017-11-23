2fc8ef67e167a4ebc739e838d74a854211d00a56
simgesen <simge.sen@bil.omu.edu.tr>
2017-11-23 11:14:39 +0300

    Add action_item

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,15 +1,37 @@
 ActiveAdmin.register Post do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  permit_params :title, :body, :published_at, :user_id
+
+  scope :all
+  scope :published
+  scope :unpublished
+
+  form do |f|
+  	inputs "Details" do 
+  		input :user
+  		input :title
+  		input :body
+  	end
+  	actions
+  end
+
+  action_item :publish, only: :show do
+	link_to "Publish", publish_admin_post_path(post), method: :put if !post.published_at?
+  end
+
+  action_item :publish, only: :show do
+	link_to "Unpublish", unpublish_admin_post_path(post), method: :put if post.published_at?
+  end
+
+  member_action :publish, method: :put do
+	 post = Post.find (params[:id])
+	 post.update(published_at: Time.zone.now)
+	 redirect_to admin_post_path(post)
+  end
+
+   member_action :unpublish, method: :put do
+	 post = Post.find (params[:id])
+	 post.update(published_at: nil)
+	 redirect_to admin_post_path(post)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :posts
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
